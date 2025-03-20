fdfcd1e555d8fa00bb8d4f3cb9cd5d8373bdfe6e
oskarcokl <oskar.cokl@gmail.com>
2025-03-20 09:21:37 +0100

    feat: Server port nov read from .env file

--- product-catalog-service/src/index.ts
+++ product-catalog-service/src/index.ts
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes';
 
 // Maybe the server should be a service? Or be inside the routes directory
 const app = express();
 // TODO: move to ENV variable, SERVER_PORT
+
+dotenv.config({
+    path: '.env.development',
+});
 
 app.use('/api', routes);
 
@@ -12,7 +17,7 @@ app.use('/api', routes);
 // endpoint needs to be publicly accessible. Maybe I can just have an API
 // gateway of sorts for serving these files and the service just needs to take
 // care of storing and linking products to them correctly.
-const port = 3400;
+const port = process.env.SERVER_PORT ?? 3000;
 app.listen(port, () => {
     console.log('Product catalog service listening on port', port);
 });
