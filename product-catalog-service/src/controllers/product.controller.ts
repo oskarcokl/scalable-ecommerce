75540b614ef5310a09c65c1276d51dac378be44c
oskarcokl <oskar.cokl@gmail.com>
2025-03-28 14:24:18 +0100

    feat: Can now query all producst of specific categories

--- product-catalog-service/src/controllers/product.controller.ts
+++ product-catalog-service/src/controllers/product.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import db from '../services/database';
-import { products } from '../db/schema';
-import { eq } from 'drizzle-orm';
+import { categories, products, productsView } from '../db/schema';
+import { eq, sql } from 'drizzle-orm';
 import { createInsertSchema, createUpdateSchema } from 'drizzle-zod';
 import {
     BadRequestError,
@@ -13,11 +13,55 @@ import { ZodError } from 'zod';
 const productInsertSchema = createInsertSchema(products);
 const productUpdateSchema = createUpdateSchema(products);
 
-export const getAllProducts = async (req: Request, res: Response) => {
-    const categoryId = req.query.category ? parseInt(req.query.category as string) : null;
+const getCategoryHierarchy = async (categoryId: number, categoryHierarchy: any[] = []) => {
+    console.log('getCategoryHierarchy');
+    const result = await db.select().from(categories).where(eq(categories.categoryId, categoryId));
 
-    const productQuery = db.select().from(products);
-    res.json(await db.select().from(products));
+    if (result.length !== 0) {
+        const data = { categoryName: result[0].categoryName, categoryId: result[0].categoryId };
+        categoryHierarchy.push(data);
+
+        if (result[0].parentCategoryId && result[0].parentCategoryId != result[0].categoryId) {
+            categoryHierarchy = await getCategoryHierarchy(
+                result[0].parentCategoryId,
+                categoryHierarchy
+            );
+        }
+    }
+
+    return categoryHierarchy;
+};
+
+export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const categoryId = req.query.category ? parseInt(req.query.category as string) : null;
+
+        const query = db.select().from(productsView);
+        const whereClauses: any[] = [];
+
+        if (categoryId) {
+            whereClauses.push(eq(productsView.categoryId, categoryId));
+        }
+
+        if (whereClauses.length > 0) {
+            query.where(sql.join(whereClauses, sql` AND `));
+        }
+
+        const productResults = await query;
+
+        let categoryHierarchy = null;
+        if (categoryId) {
+            categoryHierarchy = await getCategoryHierarchy(categoryId);
+            categoryHierarchy.reverse();
+        }
+
+        res.json({
+            products: productResults,
+            categoryHierarchy: categoryHierarchy,
+        });
+    } catch (error) {
+        return next(error);
+    }
 };
 
 export const getProductById = async (req: Request, res: Response, next: NextFunction) => {
